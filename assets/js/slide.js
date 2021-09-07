04db9354deb9742deae250679e63dcb0e02c792b
minyoung0 <81130005+minyoung0@users.noreply.github.com>
2021-09-07 22:20:09 +0900

    0907
    오류수정

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,18 +1,68 @@
 var slideIndex = 1;
 showSlides(slideIndex);
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+var slideIndex3 = 1;
+showSlides3(slideIndex3);
+///////////////////////////////////
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
+}
+function plusSlides3(n) {
+  showSlides3(slideIndex += n);
+}
+/////////////////////////////
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);  
+}
 
+function currentSlide3(n) {
+  showSlides3(slideIndex = n);  
+}
+///////////////////////////////
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function showSlides3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides3");
+  var dots = document.getElementsByClassName("dot3");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
